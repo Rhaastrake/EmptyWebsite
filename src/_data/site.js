8e507fd4f7f882d39ab193a73d0a071574dcecc0
Rhaastrake <garofalomichele2001@gmail.com>
2026-02-17 20:11:16 +0100

    Added dinamic seo for pages

---
+++ src/_data/site.js
@@ -0,0 +1,21 @@
+module.exports = {
+  homepage: {
+    seo: {
+      title: "Page title",
+      description: "Description",
+      keywords: "First keyword, second keyword",
+    },
+  },
+  anotherPage: {
+    seo: {
+      title: "Page title",
+      description: "Description",
+      keywords: "First keyword, second keyword",
+    },
+  },
+  404: {
+    seo: {
+      title: "Page title",
+    },
+  },
+};
