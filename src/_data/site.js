f4174eba5fe76329be827acbaa0c0806adf10cfd
Rhaastrake <garofalomichele2001@gmail.com>
2026-02-18 20:12:12 +0100

    Fixed dinamic title, desc and keywords

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,23 @@
 module.exports = {
+    // site_name: "Webfixer",
+  // title: "Webfixer.it | Assistenza tenica per PC e sistemi informatici",
+  // description:
+  //   "Sito vetrina di Michele Garofalo, specializzato in software e assistenza hardware di vario genere",
+  // domain: "webfixer.it",
+  // url: "https://webfixer.it", // It WILL NOT end with a slash /
+  // lang: "it",
+  // author: "Michele Garofalo",
+  // logo: "/assets/img/logo",
+  // image_width: [320, 480, 720, 1280, 1920, 2048, 2560, 3840, 4096, 7680],
+  // copyright: {
+  //   year: "2026",
+  //   text: "Michele Garofalo - Tutti i diritti riservati",
+  //   privacy: "",//link alla privacy
+  //   cookie: "",//link ai cookie
+  //   cookieControls:
+  //     "",//link ai cookie controls
+  //   terms: "",//Link ai termini
+  // },
   homepage: {
     seo: {
       title: "Page title",
