9ca9c66afabb6c98ea9e63565810730d809ecf03
FrancescoAmoruso1612 <famoruso@studenti.apuliadigitalmaker.it>
2026-03-06 16:25:55 +0100

    Unified javascript files per page in _site compilation

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,22 @@
+const esbuild = require("esbuild");
+const glob = require("glob");
+
 module.exports = function (eleventyConfig) {
-  
-  // ✅ Copia diretta delle cartelle
+
+  eleventyConfig.on("eleventy.before", async () => {
+    const entryPoints = glob.sync("src/js/pages/*.js");
+
+    await esbuild.build({
+      entryPoints,
+      bundle: true,
+      outdir: "_site/js/pages",
+      minify: true,
+    });
+  });
+
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/php/!(configExample.php)");
-
-  // ✅ File di configurazione
-  // Utili per l'indicizzazione
-
-  // eleventyConfig.addPassthroughCopy("src/manifest.json");
-  // eleventyConfig.addPassthroughCopy("src/browserconfig.xml");
-  // eleventyConfig.addPassthroughCopy("src/robots.txt");
-
-  // ✅ Copia tutto il contenuto di public nella root
-  // eleventyConfig.addPassthroughCopy({ "src/public": "/" });
 
   return {
     dir: {
--- src/js/pages/homepage.js
+++ src/js/pages/homepage.js
@@ -3,10 +3,9 @@
 //==========================
 
 import { showNotification } from '../modules/notification.js';
-// import { normalizePhoneNumber } from "../modules/normalizePhoneNumber.js";
+import { normalizePhoneNumber } from "../modules/normalizePhoneNumber.js";
 
 showNotification("Homepage notification")
-// normalizePhoneNumber()
 
 //==========================
 // "homepage" PAGE CUSTOM JAVASCRIPT INSTRUCTIONS
